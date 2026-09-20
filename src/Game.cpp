2107b056f03828b5235d37905b8867fba9302b2a
SlliperySwan <eraldobotelhojr@gmail.com>
2026-09-20 00:27:02 -0300

    refactor: Mudanças na construção de components em Game.hpp para refletir refatorações prévias

--- src/Game.cpp
+++ src/Game.cpp
@@ -26,6 +26,7 @@ Camera Game::camera;
 	
 auto& newPlayer(manager.addEntity());
 auto& wall(manager.addEntity());
+auto& vaga(manager.addEntity());
 
 int mapSizeX,mapSizeY;
 const std::string mapfile = "../assets/textures/tiles/tile_map_1.png";
@@ -82,13 +83,13 @@ void Game::init(const char * title, int xpos, int ypos, int width, int height, b
 	
 	newPlayer.addComponent<TransformComponent>(0,0,0.0f,/*220,118*/180,88,1);
 	newPlayer.addComponent<SpriteComponent>("../assets/textures/entities/carro.png",true);
-	newPlayer.addComponent<ColliderComponent>("player",Polygon{{-79,-32},{79,-32},{79,32},{-79,32}});
+	newPlayer.addComponent<ColliderComponent>("player",158,64);
 	
 	newPlayer.addComponent<KeyboardController>();
 	
 	CarMovementConfig carConfig(260.0f,1102.0f,104.0f,48.0f,27.5f,15.8f,0.92f,900.0f,800.0f,6500.0f,
-								5.0f,3.0f,0.7f,12.0f,18.0f,120.0f,0.45f,
-								38.0f,0.055f,1.15f,24.0f,45.0f,45.0f,22.0f,22.0f,
+								5.0f,3.0f,0.7f,12.0f,18.0f,120.0f,0.10f,
+								38.0f,0.025f,1.15f,1.0f,45.0f,45.0f,22.0f,22.0f,
 								Vector2D(),Vector2D(),Vector2D(),0.0f,0.0f,
 								TorqueCurve(157.0f,70.0f,4000.0f,1.0f,6000.0f),
 								GearBox({-3.545f,0.0f,3.727f,2.048f,1.393f,1.029f,0.795f},4.067f),
@@ -104,8 +105,12 @@ void Game::init(const char * title, int xpos, int ypos, int width, int height, b
 	
 	wall.addComponent<TransformComponent>(200,200,45.0f,200,300,1);
 	wall.addComponent<SpriteComponent>("../assets/textures/entities/brick_wall.png");
-	wall.addComponent<ColliderComponent>("wall",Polygon{{-100,-150},{100,-150},{100,150},{-100,150}});
+	wall.addComponent<ColliderComponent>("wall");
 	wall.addGroup(groupMap);
+
+	vaga.addComponent<TransformComponent>(-200,-200,0.0f,637,90,1);
+	vaga.addComponent<SpriteComponent>("../assets/textures/features/vaga.png");
+	vaga.addGroup(groupGround);
 }
 
 void Game::handleEvents() {
